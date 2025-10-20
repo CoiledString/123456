d107c08b664469f20268a828626965e995848475
CoiledString <68668823+CoiledString@users.noreply.github.com>
2025-10-20 08:50:29 +0100

    Update output.py

--- esphome/components/mp3player/output.py
+++ esphome/components/mp3player/output.py
@@ -1,13 +1,17 @@
-from esphome.components import output
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import output
+from esphome.const import CONF_ID
 
-mp3player_ns = cg.esphome_ns.namespace("mp3player")
-MP3PlayerOutput = mp3player_ns.class_("MP3PlayerOutput", cg.Component, output.BinaryOutput)
+empty_binary_output_ns = cg.esphome_ns.namespace('empty_binary_output')
+EmptyBinaryOutput = empty_binary_output_ns.class_('EmptyBinaryOutput', output.BinaryOutput,
+                                  cg.Component)
 
-CONFIG_SCHEMA = output.BINARY_OUTPUT_SCHEMA.extend({}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = output.BINARY_OUTPUT_SCHEMA.extend({
+    cv.Required(CONF_ID): cv.declare_id(EmptyBinaryOutput),
+}).extend(cv.COMPONENT_SCHEMA)
 
-async def to_code(config):
-    var = cg.new_Pvariable(config[output.CONF_ID])
-    await output.register_output(var, config)
-    await cg.register_component(var, config)
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield output.register_output(var, config)
+    yield cg.register_component(var, config)
